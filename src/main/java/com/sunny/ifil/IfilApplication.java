e65e3201c34f637353c86dd81bee621a42adf03e
韩文杰a <hanwja@fotile.com>
2019-12-25 22:11:56 +0800

    ifil,freemarker+springboot单体项目

---
+++ src/main/java/com/sunny/ifil/IfilApplication.java
@@ -0,0 +1,13 @@
+package com.sunny.ifil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfilApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IfilApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sunny/ifil/IfilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.ifil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfilApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
