148b3e628211b36636c29ce55e1c7beed5d777b0
Zero656329 <hwj656329@163.com>
2019-12-28 23:20:54 +0800

    shiro

---
+++ src/main/java/com/sunny/ifil/controller/BookController.java
@@ -0,0 +1,15 @@
+package com.sunny.ifil.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class BookController {
+    @GetMapping("/book")
+    public ModelAndView books(){
+        ModelAndView mv=new ModelAndView();
+        mv.setViewName("index");
+        return mv;
+    }
+}
